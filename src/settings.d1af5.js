3e4fbe13f401cc9c94dd386e13a21418ea50b820
ZeroUltra <929288107@qq.com>
2021-05-21 17:11:13 +0800

    “0.0.1”

---
+++ src/settings.d1af5.js
@@ -0,0 +1,88 @@
+window._CCSettings = {
+    platform: "web-mobile",
+    groupList: [
+        "deafult",
+        "Wall",
+        "Hero",
+        "Enemy",
+        "EnemyBullet",
+        "HeroBullet",
+        "Roudan"
+    ],
+    collisionMatrix: [
+        [
+            false,
+            false,
+            false,
+            false
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            true
+        ],
+        [
+            false,
+            true,
+            true,
+            true,
+            false,
+            true,
+            false
+        ],
+        [
+            false,
+            true,
+            false,
+            true,
+            false,
+            false,
+            false
+        ]
+    ],
+    hasResourcesBundle: true,
+    hasStartSceneBundle: false,
+    remoteBundles: [],
+    subpackages: [],
+    launchScene: "db://assets/start.fire",
+    orientation: "portrait",
+    debug: true,
+    jsList: [
+        "assets/Script/UIFrame/lib/mobx.7fec3.js"
+    ],
+    bundleVers: {
+        internal: "d40d2",
+        DragonBone: "64d99",
+        Pics: "33a0e",
+        resources: "4df2e",
+        main: "33a85"
+    }
+};
