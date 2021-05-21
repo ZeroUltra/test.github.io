98e04885cf71c4004e360376d8cae738ea84547e
ZeroUltra <929288107@qq.com>
2021-05-21 17:31:36 +0800

    “0.0.2”

---
+++ src/settings.58da4.js
@@ -0,0 +1,1 @@
+window._CCSettings={platform:"web-mobile",groupList:["default"],collisionMatrix:[[true]],hasResourcesBundle:true,hasStartSceneBundle:false,remoteBundles:[],subpackages:[],launchScene:"db://assets/scene/game_login.fire",orientation:"",jsList:[],bundleVers:{internal:"69b9a",resources:"32cd0",main:"0fd9c"}};
